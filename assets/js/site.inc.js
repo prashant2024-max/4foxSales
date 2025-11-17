4dee5e635f018f7374114c09900a9404c3b49e82
SahilGite4fox <sahilgite.4fox@gmail.com>
2025-11-17 13:12:07 +0530

    sections updated Desktop only

--- assets/js/site.inc.js
+++ assets/js/site.inc.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-  
+
   // Sticky Header
   $(window).on("scroll", function () {
     $("header").toggleClass("stickyHead", $(this).scrollTop() > 30);
   });
 
-  
+
   // Hamburger menu toggle
   $(".menuIcon").on("click", function () {
     $(".menuCont").toggleClass("active");
@@ -26,54 +26,54 @@ $(document).ready(function () {
 
   // $(document).ready(function () {
   // First Marquee - Left to Right
-    $(".psMarquee1Box").slick({
-      slidesToShow: 8,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 0,
-      speed: 3000,
-      cssEase: "linear",
-      infinite: true,
-      arrows: false,
-      dots: false,
-      pauseOnHover: false,
-      variableWidth: false,
-      responsive: [
-        { breakpoint: 1400, settings: { slidesToShow: 5 } },
-        { breakpoint: 1300, settings: { slidesToShow: 4 } },
-        { breakpoint: 850, settings: { slidesToShow: 4 } },
-        { breakpoint: 611, settings: { slidesToShow: 3 } },
-        { breakpoint: 500, settings: { slidesToShow: 2 } },
-      ],
-    });
+  $(".psMarquee1Box").slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 3000,
+    cssEase: "linear",
+    infinite: true,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    variableWidth: false,
+    responsive: [
+      { breakpoint: 1400, settings: { slidesToShow: 5 } },
+      { breakpoint: 1300, settings: { slidesToShow: 4 } },
+      { breakpoint: 850, settings: { slidesToShow: 4 } },
+      { breakpoint: 611, settings: { slidesToShow: 3 } },
+      { breakpoint: 500, settings: { slidesToShow: 2 } },
+    ],
+  });
 
-    // Second Marquee - Right to Left (reverse direction)
-    $(".psMarquee2Box").slick({
-      slidesToShow: 8,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 0,
-      speed: 3000,
-      cssEase: "linear",
-      infinite: true,
-      arrows: false,
-      dots: false,
-      pauseOnHover: false,
-      variableWidth: false,
-      rtl: true, // 👈 this makes it scroll in the opposite direction
-      responsive: [
-        { breakpoint: 1400, settings: { slidesToShow: 5 } },
-        { breakpoint: 1300, settings: { slidesToShow: 4 } },
-        { breakpoint: 850, settings: { slidesToShow: 4 } },
-        { breakpoint: 611, settings: { slidesToShow: 3 } },
-        { breakpoint: 500, settings: { slidesToShow: 2 } },
-      ],
-    });
-// });
+  // Second Marquee - Right to Left (reverse direction)
+  $(".psMarquee2Box").slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 3000,
+    cssEase: "linear",
+    infinite: true,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    variableWidth: false,
+    rtl: true, // 👈 this makes it scroll in the opposite direction
+    responsive: [
+      { breakpoint: 1400, settings: { slidesToShow: 5 } },
+      { breakpoint: 1300, settings: { slidesToShow: 4 } },
+      { breakpoint: 850, settings: { slidesToShow: 4 } },
+      { breakpoint: 611, settings: { slidesToShow: 3 } },
+      { breakpoint: 500, settings: { slidesToShow: 2 } },
+    ],
+  });
+  // });
 
   $(".psMarquee1Box, .psMarquee2Box").slick("slickPlay");
 
-  
+
 
   $(".winMiddle").slick({
     slidesToShow: 1,
@@ -94,5 +94,21 @@ $(document).ready(function () {
       { breakpoint: 611, settings: { slidesToShow: 3 } },
       { breakpoint: 500, settings: { slidesToShow: 2 } },
     ],
+  });
+
+  $('.trustedBySliderLft').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    asNavFor: '.trustedBySliderRgt'
+  });
+  $('.trustedBySliderRgt').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.trustedBySliderLft',
+    dots: false,
+    fade: true,
   });
 });
